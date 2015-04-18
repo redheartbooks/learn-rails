84c8be80f914230ff21d4e57631a38ee5215f78c
James Hamilton <support@redheartbooks.com>
2015-04-17 23:38:01 -0700

    add mailing list

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,9 +1,24 @@
 class VisitorsController < ApplicationController
 
   def new
-    Rails.logger.debug 'DEBUG: entering new method'
-    @owner = Owner.new
-    Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
+    @visitor = Visitor.new
+  end
+
+  def create
+    @visitor = Visitor.new(secure_params)
+    if @visitor.valid?
+      @visitor.subscribe
+      flash[:notice] = "Signed up #{@visitor.email}."
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def secure_params
+    params.require(:visitor).permit(:email)
   end
 
 end
